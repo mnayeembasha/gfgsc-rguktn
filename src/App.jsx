3a849491633fa03a5851071da2fb4b08690a1db3
mnayeembasha <mnayeembasha@gmail.com>
2024-08-28 22:51:23 +0530

    fixed toggle-icon issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,47 +7,47 @@ import 'aos/dist/aos.css';
 
 const teamMembers = [
   {
-    img_link: '/assets/gfgsc_rguktn_nobg.png',
+    img_link: '/assets/team/lilly.jpg',
     name: 'M.Lilly',
     position: 'ChairPerson'
   },
   {
-    img_link: '/assets/gfgsc_rguktn_nobg.png',
+    img_link: '/assets/team/harshita.jpg',
     name: 'K.Harshita',
     position: 'Vice-ChairPerson'
   },
   {
-    img_link: '/assets/gfgsc_rguktn_nobg.png',
+    img_link: '/assets/team/priyanka.jpg',
     name: 'K.Priyanka',
     position: 'Technical Lead'
   },
   {
-    img_link: '/assets/gfgsc_rguktn_nobg.png',
+    img_link: '/assets/team/mounika.jpg',
     name: 'S.Mounika',
     position: 'Public Outreach Lead'
   },
   {
-    img_link: '/assets/gfgsc_rguktn_nobg.png',
+    img_link: '/assets/team/jyothirmai.jpg',
     name: 'Ch.Jyothirmai',
     position: 'Events Lead'
   },
   {
-    img_link: '/assets/gfgsc_rguktn_nobg.png',
+    img_link: '/assets/team/divyasri.jpg',
     name: 'K.Divya Sri',
     position: 'Marketing Lead'
   },
   {
-    img_link: '/assets/gfgsc_rguktn_nobg.png',
+    img_link: '/assets/team/bhargavi.jpg',
     name: 'A.Bhargavi',
     position: 'Social Media Lead'
   },
   {
-    img_link: '/assets/gfgsc_rguktn_nobg.png',
+    img_link: '/assets/team/harika.jpg',
     name: 'D.Harika',
     position: 'Design/Branding Lead'
   },
   {
-    img_link: '/assets/gfgsc_rguktn_nobg.png',
+    img_link: '/assets/team/jayanth.jpg',
     name: 'Ch.Jayanth',
     position: 'AI/ML Lead'
   },
@@ -66,6 +66,11 @@ function App() {
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   const [openIndex, setOpenIndex] = useState(null);
 
   const toggleAccordion = (index) => {
@@ -102,84 +107,85 @@ function App() {
   return (
     <div className="bg-[#f0f8f0] text-[#1a5a1a] primary">
       {/* <!-- Header Section --> */}
-      <nav className="sticky top-0 z-50 bg-[#f0f8f0] px-4 py-1 shadow-md md:px-6">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="" className="flex items-center space-x-3">
-            <img
-              src="https://media.geeksforgeeks.org/gfg-gg-logo.svg"
-              className="h-7"
-              alt=""
+      
+    <nav className="sticky top-0 z-50 bg-[#f0f8f0] px-4 py-1 shadow-md md:px-6">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/" className="flex items-center space-x-3">
+          <img
+            src="https://media.geeksforgeeks.org/gfg-gg-logo.svg"
+            className="h-7"
+            alt=""
+          />
+          <span className="self-center text-2xl font-bold whitespace-nowrap">
+            GFGSC-RGUKTN
+          </span>
+        </a>
+        <button
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none"
+          aria-controls="navbar-solid-bg"
+          aria-expanded={isMenuOpen}
+          onClick={toggleMenu}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
             />
-            <span className="self-center text-2xl font-bold whitespace-nowrap">
-              GFGSC-RGUKTN
-            </span>
-          </a>
-          <button
-            data-collapse-toggle="navbar-solid-bg"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:border-none"
-            aria-controls="navbar-solid-bg"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-          <div
-            className="hidden w-full md:block md:w-auto"
-            id="navbar-solid-bg"
-          >
-            <ul className="flex flex-col font-medium mt-4 rounded-lg  md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-transparent">
-              <li>
-                <a
-                  href="/"
-                  className="block py-2 px-3 md:p-0 text-gray-900 rounded transition duration-200 hover:bg-[#4CAF50] md:hover:bg-transparent md:hover:text-[#4CAF50]"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#about"
-                  className="block py-2 px-3 md:p-0 text-gray-900 rounded transition duration-200 hover:bg-[#4CAF50] md:hover:bg-transparent md:hover:text-[#4CAF50]"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#team"
-                  className="block py-2 px-3 md:p-0 text-gray-900 rounded transition duration-200 hover:bg-[#4CAF50] md:hover:bg-transparent md:hover:text-[#4CAF50]"
-                >
-                  Core-Team
-                </a>
-              </li>
-              <li>
-                <a
-                  href=""
-                  className="block py-2 px-3 md:p-0 text-gray-900 rounded transition duration-200 hover:bg-[#4CAF50] md:hover:bg-transparent md:hover:text-[#4CAF50]"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
+          </svg>
+        </button>
+        <div
+          className={`${isMenuOpen ? "block" : "hidden"} w-full md:block md:w-auto`}
+          id="navbar-solid-bg"
+        >
+          <ul className="flex flex-col font-medium mt-4 rounded-lg md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-transparent">
+            <li>
+              <a
+                href="/"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded transition duration-200 hover:bg-[#4CAF50] md:hover:bg-transparent md:hover:text-[#4CAF50]"
+                aria-current="page"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded transition duration-200 hover:bg-[#4CAF50] md:hover:bg-transparent md:hover:text-[#4CAF50]"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#team"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded transition duration-200 hover:bg-[#4CAF50] md:hover:bg-transparent md:hover:text-[#4CAF50]"
+              >
+                Core-Team
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded transition duration-200 hover:bg-[#4CAF50] md:hover:bg-transparent md:hover:text-[#4CAF50]"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
 
       {/* <!-- Hero Section --> */}
       <main>
@@ -261,7 +267,7 @@ function App() {
             width="300"
             height="300"
             className="mb-4 h-40 w-40 rounded-full object-cover"
-            style={{ aspectRatio: '300/300', objectFit: 'cover' }}
+            style={{ aspectRatio: '300/300', objectFit: 'cover',objectPosition: 'top' }}
           />
           <h3 className="mb-2 text-lg font-bold">{member.name}</h3>
           <p className="text-[#1a5a1a]">{member.position}</p>
